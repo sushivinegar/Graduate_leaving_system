b90802f8a71b5c8186aea77d0f6700d6ab26401d
WdFan <3480546229@qq.com>
2020-03-07 16:07:44 +0800

    首页样式修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="scss">
+  .home {
+    height: 100%;
+    .el-container {
+      height: 100%;
+      .el-aside {
+        background: aqua;
+        height: 100%;
+      }
+      .el-main {
+        background: antiquewhite;
+      }
+    }
+  }
+</style>
